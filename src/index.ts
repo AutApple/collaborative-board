a1a5ad3c57b3be5f3ff38a813cee2861a1744f16
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-01-09 21:40:33 +0300

    implement test socket.io connection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-const message: string = 'Hello, typescript!';
-console.log(message);
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import type { Socket } from 'socket.io';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+io.on('connection', (socket: Socket) => {
+    console.log('New client connected:', socket.id);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..',  'public', 'index.html'));
+});
+
+httpServer.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
