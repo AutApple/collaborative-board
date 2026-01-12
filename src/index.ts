539b0f8c36ffa5a49af79cc33524117d31d3237f
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-01-12 18:33:55 +0300

    create root directory util

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,12 @@ import { Server } from 'socket.io';
 import type { Socket } from 'socket.io';
 
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { __rootdir } from './utils/path.utils.js';
 import type { Point } from './types/point.js';
 import { Stroke } from './types/stroke.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 
@@ -38,9 +35,9 @@ io.on('connection', (socket: Socket) => {
     })
 });
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__rootdir, 'public')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..',  'public', 'index.html'));
+  res.sendFile(path.join(__rootdir, 'public', 'index.html'));
 });
 
 httpServer.listen(process.env.APP_PORT || 3000, () => {
---
+++ src/utils/path.utils.ts
@@ -0,0 +1,8 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __rootdir = path.join(path.dirname(__filename), '..', '..'); // navigate root directory
+
+export { __rootdir }
