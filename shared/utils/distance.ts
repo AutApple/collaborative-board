f793ea63e4b35a99e077a63a3ca563e79c01482f
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-01-13 21:32:09 +0300

    use points instead of numbers in distance util

--- shared/utils/distance.ts
+++ shared/utils/distance.ts
@@ -1,3 +1,5 @@
-export function distance(x1: number, y1: number, x2: number, y2: number) {
-    return Math.hypot(x2 - x1, y2 - y1);
+import type { Point } from '../types/point.type.js';
+
+export function distance(p1: Point, p2: Point) {
+    return Math.hypot(p2.x - p1.x, p2.y - p1.y);
 }
