5a95b14efcd731eb761481c73b317b81c351eb22
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-01-16 20:14:54 +0300

    move network event handling to general

--- public/src/app.ts
+++ public/src/app.ts
@@ -1,18 +1,12 @@
 import { io } from 'socket.io-client';
 import { AppContext } from './app-context.js';
 import { EventHandler } from './event-handler/event-handler.js';
-import { NetworkEventHandler } from './network-event-handler.js';
 import { EventBus, type SemanticEventMap } from './event-bus';
 import { BoardController } from './board/board.controller.js';
 import { CameraController } from './camera/camera.controller.js';
 
 export class App {
-    private inputEventHandler: EventHandler | undefined;
-    private networkEventHandler: NetworkEventHandler | undefined;
-    
-    
     constructor(private document: Document) {}
-
     run() {
         const socket = io();
         socket.on('connect', () => {
@@ -26,17 +20,14 @@ export class App {
         const appContext = new AppContext(canvas, socket);
         const semanticEventBus = new EventBus<SemanticEventMap>();
 
-        this.networkEventHandler = new NetworkEventHandler(socket, appContext);
-        this.inputEventHandler = new EventHandler(appContext, semanticEventBus); // raw events to semantic events 
+        const eventHandler = new EventHandler(appContext, semanticEventBus); // raw events to semantic events 
+        eventHandler.registerEvents(canvas as HTMLCanvasElement, window, socket);
 
-        this.inputEventHandler.registerEvents(canvas as HTMLCanvasElement, window);
-        this.networkEventHandler.registerEvents();
 
         const boardController = new BoardController(appContext);
         const cameraController = new CameraController(appContext);
 
         boardController.subscribe(semanticEventBus);
         cameraController.subscribe(semanticEventBus);
-
     }
 }
--- public/src/event-handler/event-handler.ts
+++ public/src/event-handler/event-handler.ts
@@ -1,7 +1,10 @@
 import { BoardEventHandler } from './handlers/board.event-handler.js';
 import { CameraEventHandler } from './handlers/camera.event-handler.js';
 import type { AppContext } from '../app-context.js';
-import { EventBus, type SemanticEventMap } from '../event-bus';
+import { EventBus, SemanticEvents, type SemanticEventMap } from '../event-bus';
+import type { Socket } from 'socket.io-client';
+import { ServerBoardEvents } from '../../../shared/socket-events/board.socket-events.js';
+import type { RawBoardElement } from '../../../shared/board-elements/raw/index.js';
 export class EventHandler {
     private boardInputHandler: BoardEventHandler;
     private cameraInputHandler: CameraEventHandler;
@@ -11,7 +14,7 @@ export class EventHandler {
         this.cameraInputHandler = new CameraEventHandler(appContext, semanticEventBus);
     };
 
-    public registerEvents(canvas: HTMLCanvasElement, target: EventTarget) {
+    public registerEvents(canvas: HTMLCanvasElement, target: EventTarget, socket: Socket) {
         target.addEventListener("resize", this.handleResize.bind(this));
         canvas.addEventListener("mousedown", this.handleMouseDown.bind(this));
         canvas.addEventListener("mousemove", this.handleMouseMove.bind(this));
@@ -19,6 +22,7 @@ export class EventHandler {
         canvas.addEventListener("mouseleave", this.handleMouseLeave.bind(this));
 
         canvas.addEventListener("wheel", this.handleMouseWheel.bind(this));
+        this.registerNetworkEvents(socket);
     }
 
     public handleMouseDown(e: MouseEvent): void {
@@ -49,4 +53,10 @@ export class EventHandler {
     public handleResize() : void {
         if (this.boardInputHandler.handleResize()) return;
     }
+
+    public registerNetworkEvents(socket: Socket) {
+        socket.on(ServerBoardEvents.AddElement, (raw: RawBoardElement) => this.semanticEventBus.emit(SemanticEvents.BoardElementAdd, { rawElementData: raw }));
+        socket.on(ServerBoardEvents.RefreshBoard, (raw: RawBoardElement[]) => this.semanticEventBus.emit(SemanticEvents.BoardRefresh, { rawData: raw }));
+    }
+
 }
