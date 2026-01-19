678547059307b6dff4e83370fc6b92559581268e
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-01-19 19:49:31 +0300

    make line element utilize points array internally

--- shared/board/elements/line-board-element.ts
+++ shared/board/elements/line-board-element.ts
@@ -5,9 +5,11 @@ import type { RawLineBoardElement } from './raw/line.board-element.raw.js';
 import { BoardElementType } from './raw/types/board-element-type.js';
 
 export class LineBoardElement extends BaseBoardElement {
+    private points: Point[];
     constructor(protected pos: Point, protected pos2: Point, id?: string | undefined) {
         super(pos, id);
-        this.pos2 = pos2;
+        this.points = [];
+        this.recalculatePoints();
     }
 
     public override clone(): LineBoardElement {
@@ -15,10 +17,20 @@ export class LineBoardElement extends BaseBoardElement {
     }
 
     protected static override validatePoints(points: Point[]): boolean {
-        return !(points.length > 2 || !points[0] || !points[1])
+        return points.length <= 2;
     } 
 
-    
+    private recalculatePoints(): void {
+        // TODO: in-between points
+        this.points = [this.pos, this.pos2];
+        // console.log('Recalculated points ', this.points)
+    }
+
+    private recalculatePositions(): void {
+        this.pos = this.points[0]!;
+        this.pos2 = this.points[this.points.length-1]!;
+    }
+
     public override findClosestPointTo(worldCoords: Point): {point: Point, distance: number} {
         const distA = distance(this.pos, worldCoords);
         const distB = distance(this.pos2, worldCoords);
@@ -32,10 +44,12 @@ export class LineBoardElement extends BaseBoardElement {
 
     public setPosition2(worldCoords: Point) {
         this.pos2 = { ...worldCoords };
+        this.recalculatePoints();
     }
 
     public setPosition(worldCoords: Point) {
-        this.pos = worldCoords;
+        this.pos = { ... worldCoords };
+        this.recalculatePoints();
     }
     public getPosition() {
         return this.pos;
@@ -46,13 +60,13 @@ export class LineBoardElement extends BaseBoardElement {
     }
 
     public override getPoints(): readonly Point[] {
-        return [this.pos, this.pos2];
+        return this.points;
     }
     public override setPoints(points: Point[]) {
         if (!LineBoardElement.validatePoints(points))
                 throw Error('Wrong points array signature'); // TODO: replace with centralized messages
-        this.pos = points[0]!;
-        this.pos2 = points[1]!;
+        this.points = points; 
+        this.recalculatePositions();
     }
 
     public static override fromRaw(raw: RawLineBoardElement, id?: string | undefined) {
