bd230d806ada0846f971f3d32bb0cbe7aba3c669
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-01-25 20:29:31 +0300

    remove logs

--- public/src/network/network.ui-adapter.ts
+++ public/src/network/network.ui-adapter.ts
@@ -5,14 +5,12 @@ export class NetworkUiAdapter {
         this.disconnectOverlayElement = document.getElementById(disconnectOverlayId) as HTMLDivElement;
         this.disconnectTextElement = document.getElementById(disconnectTextId) as HTMLHeadingElement;
         if (!this.disconnectOverlayElement || !this.disconnectTextElement) throw Error('Can\'t retrieve overlay elements');
-        console.log('yo');
     }
     showDisconnectOverlay() {
         this.disconnectOverlayElement.classList.remove('disabled');
         this.disconnectTextElement.classList.remove('disabled');
     }
     hideDisconnectOverlay() {
-        console.log('yaman');
         this.disconnectOverlayElement.classList.add('disabled');
         this.disconnectTextElement.classList.add('disabled');        
     }
