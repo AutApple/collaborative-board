87ce49cd20d2df28d8ff496b2c04199e58f9aaae
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-02-08 22:26:09 +0300

    db: install and connect to prisma + init migration

---
+++ prisma/migrations/20260208192240_add_board_elements/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "BoardElement" (
+    "id" TEXT NOT NULL,
+    "data" BYTEA NOT NULL,
+
+    CONSTRAINT "BoardElement_pkey" PRIMARY KEY ("id")
+);
--- src/app-context.ts
+++ src/app-context.ts
@@ -1,11 +1,20 @@
 import { Board } from '@shared/board/board.js';
 import { RemoteCursorMap } from '../shared/remote-cursor/remote-cursor-map.js';
+import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+const client = new PrismaClient({ adapter });
 
 export class AppContext {
 	public board: Board;
 	public cursorMap: RemoteCursorMap;
+	public db: PrismaClient;
+
 	constructor() {
 		this.board = new Board();
 		this.cursorMap = new RemoteCursorMap();
+		this.db = client;
 	}
 }
