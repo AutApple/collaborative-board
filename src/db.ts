080f9e550f1774cbf1820216577313e6ad234811
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-02-25 16:16:28 +0300

    use shared env config in db initialization

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,8 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './board-app/generated/prisma/client.js';
+import { env } from '../shared/config/env.config.js';
 
-import dotenv from 'dotenv';
-
-dotenv.config();
-const connectionString = process.env.DATABASE_URL;
+const connectionString = env.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
 const dbClient = new PrismaClient({ adapter });
