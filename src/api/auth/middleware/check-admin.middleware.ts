e070a9fa39cd7656afa66b76c23099be30faf9fa
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-03-10 17:21:53 +0300

    add check admin middleware and user flags

---
+++ src/api/auth/middleware/check-admin.middleware.ts
@@ -0,0 +1,41 @@
+import type { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { env } from '../../../config/env.config.js';
+import type { AccessTokenPayload } from '../interfaces/access-token-payload.interface.js';
+import type { AnyResponseLocals } from '../../common/types/any-response-locals.type.js';
+import type { APIUserService } from '../../user/user.service.js';
+
+export interface AccessTokenResponseLocals {
+	jwtPayload: AccessTokenPayload;
+}
+
+export const checkAdmin = async (userService: APIUserService) => async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) return res.status(401).json({ message: 'Invalid credentials' });
+
+	const headerSegments = authHeader.split(' ');
+	if (headerSegments.length !== 2 || headerSegments[0] !== 'Bearer')
+		return res.status(401).json({ message: 'Invalid credentials' });
+
+	const accessToken = headerSegments[1];
+	if (!accessToken) return res.status(401).json({ message: 'Invalid credentials' });
+
+	let jwtPayload = undefined;
+	try {
+		jwtPayload = jwt.verify(accessToken, env.JWT_ACCESS_SECRET) as AccessTokenPayload;
+	} catch (err: any) {
+		return res.status(401).json({ message: 'Invalid credentials' });
+	}
+
+	if (jwtPayload === undefined) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const user = await userService.getUserById(jwtPayload.userId);
+    if (!user?.isAdmin) return res.status(403).json({ message: 'Forbidden' });
+     
+	(res.locals as AccessTokenResponseLocals & AnyResponseLocals).jwtPayload = jwtPayload;
+	next();
+};
